00a2897699aa3d65dc10ae362dddee91f2d52526
jamesaevans <james.alb.evans@gmail.com>
2019-03-25 19:07:09 +0000

    fix import error

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-import pathlib
 from setuptools import setup, find_packages
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = open("README.md").read()
 
 # This call to setup() does all the work
 setup(

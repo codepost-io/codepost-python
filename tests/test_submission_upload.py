ea9b5f98942ca8dad794ab1209a18c91439c94a4
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2019-06-01 00:09:02 -0400

    Reindented everything with 4 spaces

--- tests/test_submission_upload.py
+++ tests/test_submission_upload.py
@@ -1,10 +1,10 @@
 # Standard library imports...
 try:
-  # python 3
-  from unittest.mock import Mock, patch
+    # python 3
+    from unittest.mock import Mock, patch
 except ImportError:
-  # python 2, requires dependency
-  from mock import Mock, patch
+    # python 2, requires dependency
+    from mock import Mock, patch
 
 # Third-party imports...
 from nose.tools import assert_equal, assert_is_not_none
@@ -19,33 +19,33 @@ TEST_API_KEY = 'TEST_KEY'
 @patch('codePost_api.helpers.get_assignment_submissions')
 def test_upload_submission_with_no_conflicts(mock_get_submissions, mock_post_submission):
 
-  # create mock data
-  assignment = {
-    'id' : 1,
-  }
-
-  file1 = {
-    'code' : 'test',
-    'name' : 'test.txt',
-    'extension' : 'txt',
-  }
-
-  def side_effect(api_key, assignment_id, students, files):
-    return {
-      'assignment' : assignment_id,
-      'students' : students,
-      'files' : files,
+    # create mock data
+    assignment = {
+      'id' : 1,
     }
 
-  # setup mocks
-  mock_get_submissions.return_value = []
-  mock_post_submission.side_effect = side_effect
+    file1 = {
+      'code' : 'test',
+      'name' : 'test.txt',
+      'extension' : 'txt',
+    }
 
-  # run function
-  response = helpers.upload_submission(TEST_API_KEY, assignment, ['student1@codepost.io'], [file1])
-  answer = {
-    'assignment' : 1,
-    'students' : ['student1@codepost.io'],
-    'files' : [file1]
-  }
-  assert_equal(answer, response)
+    def side_effect(api_key, assignment_id, students, files):
+        return {
+          'assignment' : assignment_id,
+          'students' : students,
+          'files' : files,
+        }
+
+    # setup mocks
+    mock_get_submissions.return_value = []
+    mock_post_submission.side_effect = side_effect
+
+    # run function
+    response = helpers.upload_submission(TEST_API_KEY, assignment, ['student1@codepost.io'], [file1])
+    answer = {
+      'assignment' : 1,
+      'students' : ['student1@codepost.io'],
+      'files' : [file1]
+    }
+    assert_equal(answer, response)

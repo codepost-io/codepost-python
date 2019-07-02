f77e02664455ba66deca933c0948bcc78fd4b2d7
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2019-07-01 20:36:14 -0400

    Added logging submodule

---
+++ codepost/__init__.py
@@ -0,0 +1,11 @@
+"""
+codePost v2.0 SDK.
+
+Provides a convenient Python interface to the codePost API. Start scripting!
+
+   Name: codepost
+ Author: codePost Team
+  Email: team@codepost.io
+    URL: github.com/codepost-io/codepost-python
+License: Copyright (c) 2019 codePost, licensed under the MIT license
+"""

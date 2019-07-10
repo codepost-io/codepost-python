305550bb509ecc81bc9dccc3bf6502f880ccaf50
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2019-07-10 12:55:09 -0400

    Added filtering in list_submissions

--- codepost/models/assignments.py
+++ codepost/models/assignments.py
@@ -11,9 +11,11 @@ import typing as _typing
 try:
     # Python 3
     from urllib.parse import urljoin as _urljoin
+    from urllib.parse import urlencode as _urlencode
 except ImportError: # pragma: no cover
     # Python 2
     from urlparse import urljoin as _urljoin
+    from urllib import urlencode as _urlencode
 
 # Local imports
 from . import abstract as _abstract
@@ -53,16 +55,32 @@ class Assignments(
     _FIELDS_READ_ONLY = [ "rubricCategories", "mean", "median" ]
     _FIELDS_REQUIRED = [ "name", "points", "course" ]
 
-    def list_submissions(self, id=None):
+    def list_submissions(self, id=None, student=None, grader=None):
         """
-        Returns the list of submissions associated with an assignment.
+        Returns the list of submissions associated with an assignment, which
+        optionally can be filtered according to a specific submitting `student`
+        or a `grader`.
         """
         _class_type = type(self)
 
         id = self._get_id(id=id)
 
+        endpoint = "{}/submissions".format(self.instance_endpoint_by_id(id=id))
+        endpoint_params = {}
+
+        if student != None:
+            # Filter according to a specific student (will be URL-quoted later)
+            endpoint_params["student"] = student
+
+        if grader != None:
+            # Filter according to a specific grader (will be URL-quoted later)
+            endpoint_params["grader"] = grader
+
+        if len(endpoint_params) > 0:
+            endpoint += "?{}".format(_urlencode(endpoint_params))
+
         ret = self._requestor._request(
-            endpoint="{}/submissions".format(self.instance_endpoint_by_id(id=id)),
+            endpoint=endpoint,
             method="GET",
         )
         if ret.status_code == 200:

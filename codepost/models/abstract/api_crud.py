643b1528c91b548ae6353da2514a0d61e2042210
jamesaevans <james.alb.evans@gmail.com>
2019-09-30 14:07:07 -0700

    remove read_only_fields from update payload

--- codepost/models/abstract/api_crud.py
+++ codepost/models/abstract/api_crud.py
@@ -136,7 +136,10 @@ class UpdatableAPIResource(_api_resource.AbstractAPIResource):
             raise _errors.InvalidIDError()
 
         # FIXME: do kwargs validation
-        data = self._get_data_and_extend(static=True, **kwargs)
+        # Note: exclude_read_only is a form of kwargs validation (removing any read-only fields
+        # which are included). In the future, we could choose to warn they client when they
+        # attempt to update a read-only field (rather than siliently prrotecting them).
+        data = self._get_data_and_extend(static=True, exclude_read_only=True, **kwargs)
 
         ret = self._requestor._request(
             endpoint=self.instance_endpoint_by_id(id=_id),

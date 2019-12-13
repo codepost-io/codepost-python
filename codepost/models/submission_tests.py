c4bf26d51e846d308321d1d5e643f16765f18338
jamesaevans <james.alb.evans@gmail.com>
2019-12-13 15:28:35 -0800

    add autograder-related models

---
+++ codepost/models/submission_tests.py
@@ -0,0 +1,40 @@
+# =============================================================================
+# codePost v2.0 SDK
+#
+# SUBMISSION MODEL SUB-MODULE
+# =============================================================================
+
+from __future__ import print_function # Python 2
+
+# Python stdlib imports
+import typing as _typing
+
+# External dependencies
+import six as _six
+
+# Local imports
+from . import abstract as _abstract
+
+# =============================================================================
+
+@_six.add_metaclass(_abstract.APIResourceMetaclass)
+class SubmissionTests(
+    _abstract.APIResource,
+    _abstract.CreatableAPIResource,
+    _abstract.ReadableAPIResource,
+    _abstract.UpdatableAPIResource,
+    _abstract.DeletableAPIResource,
+):
+    _OBJECT_NAME = "submissionTests"
+    _FIELD_ID = "id"
+    _FIELDS = {
+        'submission': (int, 'The Submission this Submission Test corresponds to.'),
+        'testCase': (int, 'The Test Case this Submission Test corresponds to.'),
+        'logs': (str, "Any logs associated with this Submission Test."),
+        "passed": (bool, ("Whether the test run represented by this Submission Test passed.")),
+        "isError": (bool, ("Whether the test run represented by this Submission Test produced an error. Defaults to False.")),
+    }
+    _FIELDS_READ_ONLY = [ ]
+    _FIELDS_REQUIRED = [ "submission", "testCase", "logs", "passed" ]
+
+# =============================================================================
---
+++ codepost/models/test_categories.py
@@ -0,0 +1,41 @@
+# =============================================================================
+# codePost v2.0 SDK
+#
+# SUBMISSION MODEL SUB-MODULE
+# =============================================================================
+
+from __future__ import print_function # Python 2
+
+# Python stdlib imports
+import typing as _typing
+
+# External dependencies
+import six as _six
+
+# Local imports
+from . import abstract as _abstract
+
+#from . import assignments as _assignments
+from . import test_cases as _test_cases
+
+# =============================================================================
+
+@_six.add_metaclass(_abstract.APIResourceMetaclass)
+class TestCategories(
+    _abstract.APIResource,
+    _abstract.CreatableAPIResource,
+    _abstract.ReadableAPIResource,
+    _abstract.UpdatableAPIResource,
+    _abstract.DeletableAPIResource,
+):
+    _OBJECT_NAME = "testCategories"
+    _FIELD_ID = "id"
+    _FIELDS = {
+        'assignment': (int, 'The assignment this Test Category corresponds to.'),
+        'name': (str, "The name of the Test Category"),
+        'testCases': (_typing.List[_test_cases.TestCases],'The Test Cases that belong to this Test Category.'),
+    }
+    _FIELDS_READ_ONLY = [ "testCases" ]
+    _FIELDS_REQUIRED = [ "assignment", "name" ]
+
+# =============================================================================
